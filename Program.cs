b0356a72a63c099219fc4baa7e5dd150ac161e2a
ViplavMankar <viplavmankar@gmail.com>
2025-04-15 11:42:14 +0530

    Changes after final testing for Auth feature implementation

--- Program.cs
+++ Program.cs
@@ -8,39 +8,45 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var gameStoreConnectionString = "";
+var gameStoreConnectionString = string.Empty;
+var jwtKey = string.Empty;
 
 if (builder.Environment.IsDevelopment())
 {
-    if (builder.Configuration.GetConnectionString("GameStoreString") == null)
-    {
-        throw new InvalidOperationException(" is not set.");
-    }
     gameStoreConnectionString = builder.Configuration.GetConnectionString("GameStoreString");
     if (string.IsNullOrEmpty(gameStoreConnectionString))
     {
-        throw new InvalidOperationException("appsetting value not set.");
+        throw new InvalidOperationException("gamestore connection string appsetting value not set.");
+    }
+    jwtKey = builder.Configuration["Jwt:Key"];
+    if (string.IsNullOrEmpty(jwtKey))
+    {
+        throw new InvalidOperationException("jwtkey appsetting value not set.");
     }
 }
 else
 {
     if (Environment.GetEnvironmentVariable("RENDER") != null)
     {
-        // Read the PORT from environment variables (default: 5000)
         var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
         builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-        gameStoreConnectionString = Environment.GetEnvironmentVariable("GAME_STORE_CONNECTION_STRING");
 
+        gameStoreConnectionString = Environment.GetEnvironmentVariable("GAME_STORE_CONNECTION_STRING");
         if (string.IsNullOrEmpty(gameStoreConnectionString))
         {
             throw new InvalidOperationException("DATABASE_URL environment variable is not set.");
+        }
+
+        jwtKey = Environment.GetEnvironmentVariable("JWT_AUTH_KEY");
+        if (string.IsNullOrEmpty(jwtKey))
+        {
+            throw new InvalidOperationException("JWT_AUTH_KEY environment variable is not set.");
         }
     }
 }
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
-        var jwtKey = builder.Configuration["Jwt:Key"];
         options.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = false,
