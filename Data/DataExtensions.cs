516edfa871867eba3a2f5b210278a2c7a8d8aad7
ViplavMankar <viplavmankar@gmail.com>
2025-04-22 01:08:07 +0530

    Additional changes for migrations for db schema

--- Data/DataExtensions.cs
+++ Data/DataExtensions.cs
@@ -7,17 +7,17 @@ public static class DataExtensions
 {
     public static async Task MigrateDbAsync(this WebApplication app)
     {
-        //     using var scope = app.Services.CreateScope();
-        //     var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreDbContext>();
 
-        //     try
-        //     {
-        //         await dbContext.Database.MigrateAsync();
-        //         Console.WriteLine("Database migration succeeded.");
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         Console.WriteLine($"Migration failed: {ex.Message}");
-        //     }
+        try
+        {
+            await dbContext.Database.MigrateAsync();
+            Console.WriteLine("Database migration succeeded.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Migration failed: {ex.Message}");
+        }
     }
 }
